48affdc53abafa1e4519d63f8482fb76e25da6d4
MightiD <128378210+MightiD@users.noreply.github.com>
2025-08-30 12:30:58 +0100

    script for installing deps for conf files

---
+++ deps/__packages.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+sudo pacman -S hyprland hypridle hyprlock hyprshot wf-recorder swww waybar kitty dolphin wofi flatpak swaync pkgconf openssl
+
+flatpak install io.github.ungoogled_software.ungoogled_chromium
+
+#rust install for waybar music mod
+
+curl --proto '=https' --tlsv1.2 https://sh.rustup.rs -sSf | sh
+rustup update
