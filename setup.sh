66f756d5d6d33cff3218f39a23d09265c88b0bfc
MightiD <128378210+MightiD@users.noreply.github.com>
2025-08-30 20:57:13 +0100

    asdfgds

--- setup.sh
+++ setup.sh
@@ -63,9 +63,5 @@ rm -rf waybar-module-music
 
 python setup.py
 
-source ~/.zshrc
-
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
-
-source ~/.zshrc
