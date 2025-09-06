9339e47a6601b12482b21564c5f87256045814b9
MightiD <128378210+MightiD@users.noreply.github.com>
2025-09-06 19:41:22 +0100

    nvim conf

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,37 +1,45 @@
-vim.g.base46_cache = vim.fn.stdpath "data" .. "/base46/"
+vim.o.number = true
+vim.o.relativenumber = true
+vim.o.wrap = true
+vim.o.swapfile = false
+vim.o.tabstop = 4
+vim.o.shiftwidth = 4
+vim.o.expandtab = true
+vim.o.softtabstop = 4
+
 vim.g.mapleader = " "
 
--- bootstrap lazy and all plugins
-local lazypath = vim.fn.stdpath "data" .. "/lazy/lazy.nvim"
+vim.keymap.set("n", "<leader>e", ":Ex<CR>")
+vim.keymap.set("n", "<leader>p", '"+p')
+vim.keymap.set("n", "<leader>c", '"+yy')
+vim.keymap.set("v", "<leader>c", '"+y')
+--vim.keymap.set("n", "<leader>i", function()
+    --    vim.cmd("normal! mzgg=G`z")
+    --end)
+    vim.keymap.set("n", "<leader>i", vim.lsp.buf.format)
 
-if not vim.uv.fs_stat(lazypath) then
-  local repo = "https://github.com/folke/lazy.nvim.git"
-  vim.fn.system { "git", "clone", "--filter=blob:none", repo, "--branch=stable", lazypath }
-end
+    vim.pack.add({
+        {src = "https://github.com/sainnhe/gruvbox-material"},
+        {src = "https://github.com/nvim-treesitter/nvim-treesitter"},
+        {src = "https://github.com/nvim-lua/plenary.nvim"},
+        {src = "https://github.com/ThePrimeagen/harpoon", version = "harpoon2"},
+        {src = "https://github.com/mason-org/mason.nvim"},
+    })
 
-vim.opt.rtp:prepend(lazypath)
+    vim.cmd("colorscheme gruvbox-material")
 
-local lazy_config = require "configs.lazy"
+    local harpoon = require("harpoon")
+    harpoon:setup()
 
--- load plugins
-require("lazy").setup({
-  {
-    "NvChad/NvChad",
-    lazy = false,
-    branch = "v2.5",
-    import = "nvchad.plugins",
-  },
+    vim.keymap.set("n", "<leader>a", function() harpoon:list():add() end)
+    vim.keymap.set("n", "<leader>h", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
 
-  { import = "plugins" },
-}, lazy_config)
+    require'nvim-treesitter.configs'.setup {
+        ensure_installed = {"c", "lua", "python", "rust", "markdown", "javascript", "typescript"},
+        auto_install = true,
+        highlight = { enable = true, additional_vim_regex_highlighting = false,}
+    }
 
--- load theme
-dofile(vim.g.base46_cache .. "defaults")
-dofile(vim.g.base46_cache .. "statusline")
+    require("mason").setup()
 
-require "options"
-require "autocmds"
-
-vim.schedule(function()
-  require "mappings"
-end)
+    vim.lsp.enable({"lua_ls"})
