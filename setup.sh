62a3d374e0cbadc06796cef87fb966f2f430a4cf
MightiD <128378210+MightiD@users.noreply.github.com>
2026-01-01 11:39:18 +0200

    setup script changes

--- setup.sh
+++ setup.sh
@@ -1,68 +1,60 @@
 #!/bin/bash
 
-# IMPORTANT PACKAGES
-echo "INSTALLING NECESSARY PACKAGES"
-
-sudo pacman -Syy
-
-sudo pacman -S hyprland hypridle hyprlock hyprshot wf-recorder swww waybar kitty dolphin wofi flatpak swaync pkgconf openssl inetutils samba eza power-profiles-daemon python-pillow ttf-firacode-nerd ttf-font-awesome ttf-jetbrains-mono noto-fonts noto-fonts-emoji pulseaudio pulseaudio-alsa pulseaudio-bluetooth pavucontrol breeze breeze-gtk xdg-desktop-portal-gtk nwg-look ttf-hack kvantum breeze-icons breeze5 libreoffice-still openssh qt5ct qt6ct gnome-tweaks
-
-flatpak install io.github.ungoogled_software.ungoogled_chromium
-
-#rust install for waybar music mod
-
-curl --proto '=https' --tlsv1.2 https://sh.rustup.rs -sSf | sh
-rustup update
-
-sudo systemctl enable sshd
-sudo systemctl start sshd
-
-export PATH="$HOME/.cargo/bin:$PATH"
-
-# MINEGRUB THEME
-echo CONFIGURING GRUB THEME
-
-git clone https://github.com/Lxtharia/minegrub-theme.git
-
-cd minegrub-theme
-
-cp "./background_options/1.18 - [Caves And Cliffs 2].png" ./minegrub/backgrounds/
-
-sudo cp -ruv ./minegrub /boot/grub/themes/
-
-sudo echo "GRUB_THEME=/boot/grub/themes/minegrub/theme.txt" | sudo tee -a /etc/default/grub
-
-sudo grub-mkconfig -o /boot/grub/grub.cfg
-
-sudo mkdir -p /etc/pacman.d/hooks
-
-sudo cp ./templates/minegrub.hook /etc/pacman.d/hooks/minegrub.hook
-
-cd ..
-rm -rf minegrub-theme
-
-# PACMAN CONF
+# PACMAN
 echo CONFIGURING PACMAN
 
 sudo cp ./templates/pacman.conf /etc/pacman.conf
 
 sudo pacman -Syy
 
+echo "INSTALLING NECESSARY PACKAGES"
 
-# WAYBAR MODULE
-echo BUILDING WAYBAR MUSIC MODULE
+sudo pacman -S hyprland hypridle hyprlock hyprshot wf-recorder swww waybar kitty dolphin wofi flatpak swaync pkgconf openssl inetutils samba eza power-profiles-daemon python-pillow ttf-firacode-nerd ttf-font-awesome ttf-jetbrains-mono noto-fonts noto-fonts-emoji pulseaudio pulseaudio-alsa pulseaudio-bluetooth pavucontrol breeze breeze-gtk xdg-desktop-portal-gtk nwg-look ttf-hack kvantum breeze-icons breeze5 libreoffice-still openssh qt5ct qt6ct gnome-tweaks base-devel git
 
-git clone https://github.com/Andeskjerf/waybar-module-music.git
-cd waybar-module-music
-cargo build --release
 
-mkdir -p ~/.local/bin
+# YAY
+echo YAY
+git clone https://aur.archlinux.org/yay.git
+cd yay
+makepkg -si
 
-cp target/release/waybar-module-music ~/.local/bin/
+
+#BROWSER
+yay -S ungoogled-chromium-bin --noconfirm
+
+
+# RUST
+echo INSTALLING RUST
+curl --proto '=https' --tlsv1.2 https://sh.rustup.rs -sSf | sh
+rustup update
+
+export PATH="$HOME/.cargo/bin:$PATH"
+
+
+# SSH
+sudo systemctl enable sshd
+sudo systemctl start sshd
+
+
+# MINEGRUB THEME
+echo CONFIGURING GRUB THEME
+
+git clone https://github.com/Lxtharia/minegrub-theme.git
+cd minegrub-theme
+
+cp "./background_options/1.18 - [Caves And Cliffs 2].png" ./minegrub/backgrounds/
+sudo cp -ruv ./minegrub /boot/grub/themes/
+sudo echo "GRUB_THEME=/boot/grub/themes/minegrub/theme.txt" | sudo tee -a /etc/default/grub
+sudo grub-mkconfig -o /boot/grub/grub.cfg
+sudo mkdir -p /etc/pacman.d/hooks
+sudo cp ./templates/minegrub.hook /etc/pacman.d/hooks/minegrub.hook
 
 cd ..
-rm -rf waybar-module-music
+rm -rf minegrub-theme
 
+
+# HYPRLAND-SCROLLING
+echo HYPRLAND SCROLLING
 git clone https://github.com/mightid/hyprland-scrolling.git
 cd hyprland-scrolling
 cargo build --release
@@ -71,7 +63,19 @@ cd ..
 rm -rf hyprland-scrolling
 
 
+# FONTS
+echo TERMINAL FONTS
+mkdir =p ~/.local/share/fonts
+wget -O ~/.local/share/fonts/Miracode.ttf \
+    https://github.com/IdreesInc/Miracode/releases/download/v1.0/Miracode.ttf
+fc-cache -fv
+
+
+# SYM-LINKS SETUP
+echo SYM-LINKS
 python setup.py
 
+
+# ZSH PLUGINS
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
