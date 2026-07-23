ceb4ff3fbf4dd7f226baa0fd7123acf6a118c314
MightiD <128378210+MightiD@users.noreply.github.com>
2026-07-23 18:24:16 +0100

    fix syncthing

--- system/modules/syncthing.nix
+++ system/modules/syncthing.nix
@@ -3,5 +3,8 @@
     services.syncthing = {
         enable = true;
         openDefaultPorts = true;
+        user = "mightid";
+        dataDir = "/home/mightid";
+        configDir = "/home/mightid/.config/syncthing";
     };
 }
