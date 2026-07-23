1d4fdb563f5461763cd74c89728b5425a9bd1060
MightiD <128378210+MightiD@users.noreply.github.com>
2026-07-23 20:57:25 +0100

    pwaudiocontrol gui app

--- system/modules/pipewire.nix
+++ system/modules/pipewire.nix
@@ -1,7 +1,11 @@
-{ ... }:
+{ pkgs, ... }:
 {
     services.pipewire = {
         enable = true;
         pulse.enable = true;
     };
+
+    environment.systemPackages = with pkgs; [
+        pwvucontrol
+    ];
 }
