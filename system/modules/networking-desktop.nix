3f4104135f00b0fbe0b0953fac1e3c35da91e32c
MightiD <128378210+MightiD@users.noreply.github.com>
2026-07-24 08:28:38 +0100

    WoL that doesnt work hehe

--- system/modules/networking-desktop.nix
+++ system/modules/networking-desktop.nix
@@ -1,12 +1,22 @@
-{ ... }:
+{ pkgs, ... }:
 {
     imports = [
         ./networking.nix
     ];
+
     networking.nameservers = [ "192.168.255.1" ];
     networking.defaultGateway = "192.168.0.1";
-    networking.interfaces.enp9s0.ipv4.addresses = [{
-        address = "192.168.100.100";
-        prefixLength = 16;
-    }];
+    networking.interfaces.enp9s0 = {
+
+        ipv4.addresses = [{
+            address = "192.168.100.100";
+            prefixLength = 16;
+        }];
+
+        wakeOnLan.enable = true;
+    };
+
+    environment.systemPackages = with pkgs; [
+        wakeonlan
+    ];
 }
