2d914fbbdae0e0aa9fb1139b3766a4a50ab1e4fb
MightiD <128378210+MightiD@users.noreply.github.com>
2026-08-01 22:37:26 +0100

    nix shell zsh plugin

--- home/modules/zsh.nix
+++ home/modules/zsh.nix
@@ -1,9 +1,21 @@
-{ ... }:
+{ pkgs, ... }:
 {
     programs.zsh = {
         enable = true;
         autosuggestion.enable = true;
         syntaxHighlighting.enable = true;
+        plugins = [
+            {
+                name = "zsh-nix-shell";
+                file = "nix-shell.plugin.zsh";
+                src = pkgs.fetchFromGitHub {
+                    owner = "chisui";
+                    repo = "zsh-nix-shell";
+                    rev = "v0.8.0";
+                    sha256 = "1lzrn0n4fxfcgg65v0qhnj7wnybybqzs4adz7xsrkgmcsr0ii8b7";
+                };
+            }
+        ];
         oh-my-zsh = {
             enable = true;
             theme = "garyblessington";
