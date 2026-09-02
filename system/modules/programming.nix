d3008405da29c961bbc84dba7820678e6d344e25
MightiD <128378210+MightiD@users.noreply.github.com>
2026-09-02 12:15:08 +0100

    add some C and man pages things

--- system/modules/programming.nix
+++ system/modules/programming.nix
@@ -3,8 +3,15 @@
     programs.neovim.defaultEditor = true;
 
     environment.systemPackages = with pkgs; [
+        gcc
+        gnumake
+        binutils
         lazygit
         neovim
         tmux
+        man-pages
+        man-pages-posix
     ];
+
+    documentation.dev.enable = true;
 }
